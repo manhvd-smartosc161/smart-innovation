9d40b4d1d6d2a3574c80558c4aadde275977edb0
manhvd-smartosc <manhvd@smartosc.com>
2025-12-17 09:23:25 +0700

    fix: update css for scope & impact tab

--- src/components/molecules/EditableTable/index.tsx
+++ src/components/molecules/EditableTable/index.tsx
@@ -418,9 +418,11 @@ export function EditableTable<T extends Record<string, any>>({
               }}
             >
               {col.title}
-              {isSorted && sortedInfo.order === 'ascend' && <ArrowUpOutlined />}
+              {isSorted && sortedInfo.order === 'ascend' && (
+                <ArrowUpOutlined style={{ opacity: 0.45 }} />
+              )}
               {isSorted && sortedInfo.order === 'descend' && (
-                <ArrowDownOutlined />
+                <ArrowDownOutlined style={{ opacity: 0.45 }} />
               )}
             </span>
           ),
@@ -476,9 +478,36 @@ export function EditableTable<T extends Record<string, any>>({
                 )
               : undefined,
           filterIcon: (filtered) => (
-            <SearchOutlined
-              style={{ color: filtered ? '#1890ff' : undefined }}
-            />
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{ color: filtered ? '#1890ff' : 'rgba(0, 0, 0, 0.45)' }}
+            >
+              <path
+                d="M4 6H20"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M7 12H17"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M10 18H14"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           ),
           onCell: (_, rowIndex) => ({
             onClick: (e) =>
